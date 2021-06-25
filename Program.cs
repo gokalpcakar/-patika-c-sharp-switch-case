e16ef0daafd9258ce28a829196ab30c61602da60
cemil <gokalpcakar@gmail.com>
2021-06-25 18:50:22 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace patika_switch_case
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int month = DateTime.Now.Month;
+
+            // İfade
+            switch(month){
+
+                case 1:
+                    Console.WriteLine("Ocak");
+                    break;
+
+                case 2:
+                    Console.WriteLine("Şubat");
+                    break;
+
+                case 4:
+                    Console.WriteLine("Nisan");
+                    break;
+
+                case 3:
+                    Console.WriteLine("Mart");
+                    break;
+
+                default:
+                    Console.WriteLine("Yanlış veri girişi");
+                    break;
+            }
+
+            switch(month){
+
+                case 12:
+                case 1:
+                case 2:
+                    Console.WriteLine("Kış Mevsimindeyiz");
+                    break;
+
+                case 3:
+                case 4:
+                case 5:
+                    Console.WriteLine("İlkbahar Mevsimindeyiz");
+                    break;
+
+                case 6:
+                case 7:
+                case 8:
+                    Console.WriteLine("Yaz Mevsimindeyiz");
+                    break;
+
+                default:
+                    Console.WriteLine("Yanlış veri girişinde bulundunuz.");
+                    break;
+
+                case 9:
+                case 10:
+                case 11:
+                    Console.WriteLine("Sonbahar Mevsimindeyiz");
+                    break;
+            }
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Patika Sitesinin Odevi Icin Switch-Case Pratigi
+
+Patika.dev sitesi için yapmış olduğum basit bir konsol uygulaması
